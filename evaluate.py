f2a1b923b26fde2f66c350c983280b28c4673bde
Ayan Mazhitov <mazhitovayan@gmail.com>
2023-12-05 16:01:23 +0100

    Updating to new version of gym

--- evaluate.py
+++ evaluate.py
@@ -12,13 +12,14 @@ path = os.path.abspath(os.getcwd())
 name = str(path) + "/dqn.h5"
 model = keras.models.load_model(name)
 
-env = gym.make("CartPole-v1")
+env = gym.make("CartPole-v1", render_mode='human')
 done = False
-state = env.reset().reshape(1,4)
+state = env.reset()[0].reshape(1,4)
 total_reward = 0
 while not done:
+    env.render()
     action = np.argmax(model.predict(state))
-    nextState, reward, done, x = env.step(action)
+    nextState, reward, done, x, _ = env.step(action)
     nextState = nextState.reshape(1, 4)
     total_reward += reward
     state = nextState
--- train.py
+++ train.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     actions = range(action_size)
     policy_net = DQN(state_size, action_size)
     memory = ReplayBuffer(memory_capacity)
-    n_episodes = 400
+    n_episodes = 10
     n_steps = 250
     all_rewards = []
     epsilon = 0.99
@@ -28,7 +28,8 @@ if __name__ == '__main__':
     for e in range(n_episodes):
         total_reward = 0
         print("New episode #" + str(e))
-        state = env.reset().reshape(1,state_size)
+
+        state = env.reset()[0].reshape(1,state_size)
         if epsilon > 0.05:
             epsilon *= decay
         for i in range(n_steps):
@@ -37,7 +38,7 @@ if __name__ == '__main__':
             else:
                 q_values = model.predict(state)
                 action = np.argmax(q_values)
-            nextState, reward, done, x = env.step(action)
+            nextState, reward, done, x, _ = env.step(action)
             nextState = nextState.reshape(1, state_size)
             total_reward += reward
             memory.add(state, action, reward, done, nextState)
@@ -52,7 +53,7 @@ if __name__ == '__main__':
         print("Total number of steps: %2d; Total reward: %2.2f; Epsilon: %3.2f" % (i + 1, total_reward, epsilon))
     t = np.arange(n_episodes)
     path = os.path.abspath(os.getcwd())
-    name = str(path) + "/dqn.h5"
+    name = str(path) + "/dqn_v1.h5"
     policy_net.model.save(name)
     plt.plot(t,all_rewards)
     plt.show()
